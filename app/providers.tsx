212ee94e0dd6af37b0b56b3d239c28212ec7a00e
Sele <156505079+SeventhOdyssey71@users.noreply.github.com>
2025-02-26 09:34:55 +0100

    Delete app/providers.tsx

--- app/providers.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import { WalletProvider } from "@mysten/wallet-kit"
-import type { ReactNode } from "react"
-
-export function Providers({ children }: { children: ReactNode }) {
-  return <WalletProvider autoConnect={false}>{children}</WalletProvider>
-}
-
